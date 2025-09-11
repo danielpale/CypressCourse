e74dd6364b5ad80772fe4a9c6f57c17eba613a78
danielpale <62296781+danielpale@users.noreply.github.com>
2025-09-11 12:21:56 -0600

    feat: Se continuó el curso de cypress.

--- Section 03 - Diving deeper/cypress/e2e/contact.cy.js
+++ Section 03 - Diving deeper/cypress/e2e/contact.cy.js
@@ -5,11 +5,30 @@ describe("contact form", () => {
     cy.visit("http://localhost:5173/about");
     cy.get('[data-cy="contact-input-message').type("Hello World");
     cy.get('[data-cy="contact-input-name').type("John Doe");
-    cy.get('[data-cy="contact-input-email').type("john@example.com");
-    cy.get('[data-cy="contact-btn-submit').contains("Send Message");
-    cy.get('[data-cy="contact-btn-submit').should("not.have.attr", "disabled");
+
+    cy.get('[data-cy="contact-btn-submit').then((el) => {
+      expect(el.attr("disabled")).to.be.undefined;
+      expect(el.text()).to.eq("Send Message");
+    });
+
+    cy.get('[data-cy="contact-input-email').type("john@example.com{enter}");
+    // cy.get('[data-cy="contact-btn-submit')
+    //   .contains("Send Message")
+    //   .should("not.have.attr", "disabled");
+
+    cy.get('[data-cy="contact-btn-submit').as("submitBtn");
+    // cy.get("@submitBtn").click();
+    cy.get("@submitBtn").contains("Sending...");
+    cy.get("@submitBtn").should("have.attr", "disabled");
+  });
+
+  it("should validte the form input", () => {
+    cy.visit("http://localhost:5173/about");
+
     cy.get('[data-cy="contact-btn-submit').click();
-    cy.get('[data-cy="contact-btn-submit').contains("Sending...");
-    cy.get('[data-cy="contact-btn-submit').should("have.attr", "disabled");
+    cy.get('[data-cy="contact-btn-submit').then((el) => {
+      expect(el).to.not.have.attr("disabled");
+      expect(el.text()).to.not.equal("Sending...");
+    });
   });
 });
